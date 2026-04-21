2131ba38341f6c9d4ef77cde95b1f8d022c49248
Tom Lynch <twhlynch@gmail.com>
2026-04-22 00:33:54 +1000

    chore: add a test lc3 file

---
+++ test/fibonacci.asm
@@ -0,0 +1,32 @@
+; https://codeberg.org/dxrcy/elk/src/branch/master/examples/fibonacci.asm
+
+.ORIG 0x3000
+
+    lea r0, InputPrompt
+    puts
+    getc
+
+    ld r1, NegAsciiZero
+    add r1, r0, r1
+
+    and r0, r0, #0
+    add r3, r3, #1
+
+    invalid syntax!
+
+FibLoop
+    add r4, r0, r3
+    add r3, r0, #0
+    add r0, r4, #0
+
+    add r1, r1, #-1
+    brp FibLoop
+
+    putn
+
+    halt
+
+InputPrompt     .STRINGZ "Input a number 1-9: "
+NegAsciiZero    .FILL -0x30
+
+.END
