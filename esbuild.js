6027db4ae9c09ae3cc2c39650955a26d8daf76c7
Tom Lynch <twhlynch@gmail.com>
2026-04-24 03:27:37 +1000

    refactor: remove unused extra esbuild config

--- esbuild.js
+++ esbuild.js
@@ -1,32 +1,9 @@
 const esbuild = require('esbuild');
 
 const production = process.argv.includes('--production');
-const watch = process.argv.includes('--watch');
 
-/**
- * @type {import('esbuild').Plugin}
- */
-const esbuildProblemMatcherPlugin = {
-	name: 'esbuild-problem-matcher',
-
-	setup(build) {
-		build.onStart(() => {
-			console.log('[watch] build started');
-		});
-		build.onEnd((result) => {
-			result.errors.forEach(({ text, location }) => {
-				console.error(`✘ [ERROR] ${text}`);
-				console.error(
-					`    ${location.file}:${location.line}:${location.column}:`,
-				);
-			});
-			console.log('[watch] build finished');
-		});
-	},
-};
-
-async function main() {
-	const ctx = await esbuild.context({
+esbuild
+	.build({
 		entryPoints: ['src/extension.ts'],
 		bundle: true,
 		format: 'cjs',
@@ -36,18 +13,9 @@ async function main() {
 		platform: 'node',
 		outfile: 'dist/extension.js',
 		external: ['vscode'],
-		logLevel: 'silent',
-		plugins: [esbuildProblemMatcherPlugin],
+		logLevel: 'info',
+	})
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
 	});
-	if (watch) {
-		await ctx.watch();
-	} else {
-		await ctx.rebuild();
-		await ctx.dispose();
-	}
-}
-
-main().catch((e) => {
-	console.error(e);
-	process.exit(1);
-});
